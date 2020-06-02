b44cd7940051b3c7c98e6155d59f4d26b6f97741
pdavinunes <p.davi.nunes@gmail.com>
2020-06-02 20:30:14 -0300

    Criando arquivo de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    response.json({"status": "running!"})
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
